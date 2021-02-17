4ee1d6f389106ffcebce2a6b174c5cc54d8e7aef
Swivi <shenyuangongzuo@163.com>
2021-02-17 13:48:31 +0800

    Test File has changed from Loading Animation to Swing

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -72,13 +72,14 @@ int main() {
   glEnableVertexAttribArray(0);
 
   std::vector<float> dotVertices;
+  float step = 10.0f;
   float angle = 0.0f;
   float LastPos[2] = {0.0, 1.0};
   float ThisPos[2] = {0.0, 1.0};
   float R = 1.0f;
   float Center[2] = {0.0f, 0.0f};
-  while (angle <= 360 + 1.0f) {
-    angle += 1.0;
+  while (angle <= 360 + step) {
+    angle += step;
     LastPos[0] = ThisPos[0];
     LastPos[1] = ThisPos[1];
     ThisPos[0] = sin(glm::radians(angle)) * R + Center[0];
@@ -120,7 +121,7 @@ int main() {
   //============//
   // Randr Loop //
   //===================================
-  glClearColor(0.8f, 0.8f, 0.7f, 1.0f);
+  glClearColor(0.8f, 0.8f, 0.65f, 1.0f);
 
   float currentTime;
   float lastSec = glfwGetTime();
@@ -136,15 +137,41 @@ int main() {
     //===========
     shader.use();
 
+    float UpWay = 0.45f;
+
+    // UP
     shader.setFloat("Fixx", (float)screenWidth);
     shader.setFloat("Fixy", (float)screenHeight);
 
-    glm::mat4 Modul = glm::mat4(1.0f);
-    Modul = glm::translate(Modul, glm::vec3(0.0f, 0.2f, 0.0f));
-    Modul = glm::rotate(Modul, (float)glm::radians(30.0f),
+    glm::mat4 Modul = glm::mat4(1.0);
+    Modul = glm::translate(Modul, glm::vec3(0.0f, UpWay, 0.0f));
+    Modul = glm::scale(Modul, glm::vec3(0.02f, 0.02f, 1.0f));
+
+    shader.setMat4("Modul", Modul);
+
+    glBindVertexArray(dotVAO);
+    glDrawArrays(GL_TRIANGLES, 0, dotVertices.size());
+
+    // DOWN
+    float xsite, ysite = 0.0f;
+    xsite = sin(sin(3.14 * glfwGetTime())) * 0.8;
+    ysite = cos(sin(3.14 * glfwGetTime())) * 0.8;
+    ysite = ysite < 0 ? ysite : -ysite;
+
+    Modul = glm::mat4(1.0f);
+    Modul = glm::translate(Modul, glm::vec3(xsite, UpWay + ysite, 0.0));
+    Modul = glm::scale(Modul, glm::vec3(0.1f, 0.1f, 1.0));
+
+    shader.setMat4("Modul", Modul);
+    glDrawArrays(GL_TRIANGLES, 0, dotVertices.size());
+
+    // Stick
+    Modul = glm::mat4(1.0f);
+    Modul = glm::translate(Modul, glm::vec3(xsite, UpWay + ysite, 0.0f));
+    Modul = glm::rotate(Modul, (float)sin(3.14f * glfwGetTime()),
                         glm::vec3(0.0f, 0.0f, 1.0f));
-    Modul = glm::rotate(Modul, (float)glm::radians(sin(glfwGetTime()) * 180),
-                        glm::vec3(0.0f, 0.0f, 1.0f));
+    Modul = glm::translate(Modul, glm::vec3(0.0f, 0.35f, 0.0f));
+    Modul = glm::scale(Modul, glm::vec3(0.015f, 1.00f, 1.0f));
 
     shader.setMat4("Modul", Modul);
 
@@ -165,8 +192,11 @@ int main() {
     glBindVertexArray(dotVAO);
     glDrawArrays(GL_TRIANGLES, 0, dotVertices.size());
 
+    xsite = sin(sin(3.14 * glfwGetTime() / 2)) * 0.25;
+
     dotModul = glm::translate(
-        glm::mat4(1.0f), glm::vec3(sin((currentTime)*2) * 0.25, -0.8f, 0.0f));
+        glm::mat4(1.0f),
+        glm::vec3((xsite > 0.0 ? xsite : 0.0) + 0.035, -0.8f, 0.0f));
     dotModul = glm::scale(dotModul, glm::vec3(0.02f, 0.02f, 1.0f));
     dot.setMat4("Modul", dotModul);
     glBindVertexArray(dotVAO);
@@ -174,8 +204,7 @@ int main() {
 
     dotModul = glm::translate(
         glm::mat4(1.0f),
-        glm::vec3(cos((currentTime + glm::radians(45.0f)) * 2) * 0.25, -0.8f,
-                  0.0f));
+        glm::vec3((xsite < 0.0 ? xsite : 0.0) - 0.035, -0.8f, 0.0f));
     dotModul = glm::scale(dotModul, glm::vec3(0.02f, 0.02f, 1.0f));
     dot.setMat4("Modul", dotModul);
     glBindVertexArray(dotVAO);
